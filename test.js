e79144f173d3714b9cb921723bac01788e50df9f
agurishimabukuro <aguri27@hotmail.com>
2022-06-09 11:57:16 -0300

    Changes65

---
+++ test.js
@@ -0,0 +1,7 @@
+function calculateDaysBetweenDates(begin, end){
+    var beginDate = new Date(begin);
+    var endDate = new Date(end);
+    var days = Math.round
+}
+
+calculateDaysBetweenDates("01/01/2014", "01/02/2014");
